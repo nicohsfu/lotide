c30e77e8c10076c38c8f3270df7469eed3b210b6
nicohsfu <marcnicohernandez@gmail.com>
2022-09-19 21:02:19 -0700

    adding a head function to the original assertEqual js

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(5, 6);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
