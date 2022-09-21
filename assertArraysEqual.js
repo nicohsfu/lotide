0e1ecb86fa11ed9adfb91156953adc72ea2e5cc5
nicohsfu <marcnicohernandez@gmail.com>
2022-09-20 18:32:38 -0700

    fixed eqArrays so true and false are booleans and not strings, then created new function

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+// old function from a previous activity
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+
+  return result;
+};
+
+// new function below
+const assertArraysEqual = function(arr1, arr2) {
+
+  let comparison = eqArrays(arr1, arr2);
+
+  if (comparison) {
+    console.log("this is true");
+  } else {
+    console.log("this is false");
+  }
+
+};
+
+//tests
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+assertArraysEqual([1, 2, 3], [1, 2, "3"]); //false
--- eqArrays.js
+++ eqArrays.js
@@ -14,7 +14,7 @@ const eqArrays = function(arr1, arr2) {
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      result = "false";
+      result = false;
     }
   }
 
