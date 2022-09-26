61bd42c9affd7308ba568f26cf685e4f0420348a
nicohsfu <marcnicohernandez@gmail.com>
2022-09-26 12:50:05 -0700

    refactoring eqArrays by moving out its test code

--- eqArrays.js
+++ eqArrays.js
@@ -19,11 +19,4 @@ const eqArrays = function(arr1, arr2) {
   return result;
 };
 
-// tests
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should fail
+module.exports = eqArrays;
